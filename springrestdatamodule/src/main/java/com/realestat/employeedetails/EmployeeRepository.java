df632b4aeaea184c5095288a5bf26ca0712b0ead
ShireenMatte <64984165+ShireenMatte@users.noreply.github.com>
2020-09-13 23:48:39 +0400

    Repository class file

---
+++ springrestdatamodule/src/main/java/com/realestat/employeedetails/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.realesta.employeedetails;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author Shireenbanu
+ */
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
