45f253cd3a607d781eeae3c4ea2985a49b1bb380
ShireenMatte <64984165+ShireenMatte@users.noreply.github.com>
2020-09-14 00:53:40 +0400

    Update Application.java

--- springrestdatamodule/src/main/java/com/realestat/employeedetails/Application.java
+++ springrestdatamodule/src/main/java/com/realestat/employeedetails/Application.java
@@ -1,4 +1,4 @@
-package com.realesta.employeedetails;
+package com.realestat.employeedetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
