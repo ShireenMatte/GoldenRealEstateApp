d0c9478eee84882959fd0cc7e15685adb6a5944a
ShireenMatte <64984165+ShireenMatte@users.noreply.github.com>
2020-09-14 00:54:31 +0400

    Update DatabaseLoader.java

--- springrestdatamodule/src/main/java/com/realestat/employeedetails/DatabaseLoader.java
+++ springrestdatamodule/src/main/java/com/realestat/employeedetails/DatabaseLoader.java
@@ -1,4 +1,4 @@
-package com.realesta.employeedetails;
+package com.realestat.employeedetails;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
